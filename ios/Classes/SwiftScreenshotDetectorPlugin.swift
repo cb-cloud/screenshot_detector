2e05f81fab028b69e1481bd8d5c3c9a6a85d492c
Kurogoma4D <highdro0479@gmail.com>
2021-01-27 18:32:04 +0900

    add: iOS support

--- ios/Classes/SwiftScreenshotDetectorPlugin.swift
+++ ios/Classes/SwiftScreenshotDetectorPlugin.swift
@@ -2,13 +2,55 @@ import Flutter
 import UIKit
 
 public class SwiftScreenshotDetectorPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "screenshot_detector", binaryMessenger: registrar.messenger())
-    let instance = SwiftScreenshotDetectorPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
-  }
+    
+    static var channel: FlutterMethodChannel?
+    static var observer: NSObjectProtocol?
+    
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let methodChannel = FlutterMethodChannel(name: "cb-cloud/screenshot_detector", binaryMessenger: registrar.messenger())
+        observer = nil
+        let instance = SwiftScreenshotDetectorPlugin()
+        
+        channel = methodChannel
+        registrar.addMethodCallDelegate(instance, channel: methodChannel)
+    }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "initialize":
+            removeScreenShotObserverIfNeeded()
+            
+            SwiftScreenshotDetectorPlugin.observer = NotificationCenter.default.addObserver(forName: UIApplication.userDidTakeScreenshotNotification, object: nil, queue: .main) { notification in
+                
+                if let channel = SwiftScreenshotDetectorPlugin.channel {
+                    channel.invokeMethod("onCallback", arguments: nil)
+                }
+            }
+            result("initialize")
+        case "activate":
+            result("activate")
+            
+        case "inactivate":
+            result("inactivate")
+            
+        case "dispose":
+            removeScreenShotObserverIfNeeded()
+            result("dispose")
+            
+        default:
+            result("")
+        }
+    }
+    
+    
+    private func removeScreenShotObserverIfNeeded() {
+        if (SwiftScreenshotDetectorPlugin.observer != nil) {
+            NotificationCenter.default.removeObserver(SwiftScreenshotDetectorPlugin.observer!)
+            SwiftScreenshotDetectorPlugin.observer = nil;
+        }
+    }
+    
+    deinit {
+        removeScreenShotObserverIfNeeded()
+    }
 }
